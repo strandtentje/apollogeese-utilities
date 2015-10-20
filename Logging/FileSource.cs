e9abaca8b17a7f23feeb4b1981fee921f4b38b22
Rob Tierolff <teh@borreh.nl>
2015-10-20 23:02:26 +0200

    Loggin renamed to IO; introduced 'FileSource'

---
+++ Logging/FileSource.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+
+namespace Borrehsoft.Utensils.IO
+{
+	public class FileSource
+	{
+		protected string FilePath;
+		DateTime LastDate = DateTime.MaxValue; // this will absolutely break something
+		string CachedText;
+
+		public FileSource (string filePath)
+		{
+			this.FilePath = filePath;
+		}
+
+		public override string GetText() {
+			DateTime CurrentDate = File.GetLastWriteTime (this.FilePath);
+			if (!CurrentDate.Equals (LastDate)) {
+				this.LastDate = CurrentDate;
+				this.CachedText = File.ReadAllText (this.FilePath);
+			}
+			return this.CachedText;
+		}
+	}
+}
+
