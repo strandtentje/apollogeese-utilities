a11f780a37b802443048615fb07771a55bae0c32
Rob Tierolff <teh@borreh.nl>
2015-11-08 20:45:50 +0100

    Moved generic pool to Collection Utilities.

---
+++ Collections/Pool.cs
@@ -0,0 +1,79 @@
+using System;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.Utensils.Collections.Maps;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections.Generic;
+
+namespace BorrehSoft.Utensils.Collections
+{
+	class Pool<T> : IDisposable
+	{
+		public Func<T> Constructor {
+			get;
+			private set;
+		}
+
+		public Action<T> Deconstructor {
+			get;
+			private set;
+		}
+
+
+
+		public Func<T, bool> Checker {
+			get;
+			private set;
+		}
+
+		private bool DefaultChecker(T a) {
+			return true;
+		}
+
+		public Queue<T> UnderlyingPool { get; private set; }
+
+		FifoPhore underlyingSignalling;
+
+		public int PoolSize {
+			get {
+				return UnderlyingPool.Count;
+			}
+			private set {
+				UnderlyingPool = new Queue<T> (value);
+				underlyingSignalling = new FifoPhore (value);
+
+				for (int i = 0; i < value; i++) {
+					UnderlyingPool.Enqueue (Constructor ());
+				}
+			}
+		}
+
+		public Pool(int poolsize, Func<T> constructor, Action<T> deconstructor, Func<T, bool> checker = null) {
+			this.Constructor = constructor;
+			this.Deconstructor = deconstructor;
+			this.Checker = DefaultChecker;
+			if (checker != null)
+				this.Checker = DefaultChecker;
+			this.PoolSize = poolsize;
+		}
+
+		public void Fetch (Action<T> callback)
+		{
+			underlyingSignalling.WaitOne ();
+			T candidate = UnderlyingPool.Dequeue ();
+			if (!Checker (candidate)) {
+				candidate = Constructor ();
+			}
+			callback (candidate);
+			UnderlyingPool.Enqueue (candidate);
+			underlyingSignalling.Release ();
+		}
+
+		public void Dispose() {
+
+		}
+	}
+
+}
+
