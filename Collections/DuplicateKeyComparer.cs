1d8dbb5ead55b39278c2f2438583994c957e18ce
Rob Tierolff <teh@borreh.nl>
2016-10-31 14:58:08 +0100

    Comparers for non-unique collections

---
+++ Collections/DuplicateKeyComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+	/// <summary>
+	/// Comparer for comparing two keys, handling equality as beeing greater
+	/// Use this Comparer e.g. with SortedLists or SortedDictionaries, that don't allow duplicate keys
+	/// </summary>
+	/// <typeparam name="TKey"></typeparam>
+	public class DuplicateKeyComparer<TKey> : IComparer<TKey> where TKey : IComparable
+	{
+		// you saw it here first: http://stackoverflow.com/questions/5716423
+		public virtual int Compare (TKey x, TKey y)
+		{
+			int result = x.CompareTo (y);
+
+			if (result == 0)
+				return 1;   // Handle equality as beeing greater
+		else
+				return result;
+		}
+	}
+}
---
+++ Collections/ReverseDuplicateKeyComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Collections
+{
+	public class ReverseDuplicateKeyComparer<TKey> : DuplicateKeyComparer<TKey> where TKey : IComparable
+	{
+		public override int Compare (TKey x, TKey y)
+		{
+			return -base.Compare (x, y);
+		}
+	}
+}
+
