ae097295b97e83df15ffb9169ce6b6bd972566d8
Rob Tierolff <teh@borreh.nl>
2016-11-01 15:28:00 +0100

    Moved thread-aware profiler here

---
+++ Logging/Profiling/ThreadAware/ThreadAwareProfiler.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace BorrehSoft.Utensils.Log.Profiling
+{
+	public class ThreadAwareProfiler
+	{
+		public ThreadAwareProfiler() {
+			this.CorrectForNesting = true;
+		}
+
+		long ticksSpent = 0;
+
+		long measurementsMade = 0;
+
+		public long TotalTicksSpent { get { return this.ticksSpent; } }
+
+		public long MeasurementCount { get { return this.measurementsMade; } }
+
+		public long TicksPerMeasurement { get { return this.TotalTicksSpent / Math.Max(1, this.MeasurementCount); } }
+
+		private static ListDictionary threadStopwatches = new ListDictionary ();
+
+		public bool CorrectForNesting { get; set; }
+
+		public void Reset ()
+		{
+			this.ticksSpent = 0; 
+			this.measurementsMade = 0;
+		}
+
+		public void Measure(Action runMeasurementSubject) {
+
+			int threadId = Thread.CurrentThread.ManagedThreadId;
+			bool unstackProfiler = threadStopwatches.Contains (threadId) && this.CorrectForNesting;
+			Stopwatch previousStopwatch = null;
+			Stopwatch stopwatch = new Stopwatch ();
+
+			if (unstackProfiler) {
+				previousStopwatch = (Stopwatch)threadStopwatches [threadId];
+				previousStopwatch.Stop ();
+				threadStopwatches.Remove (threadId);
+			}
+
+			if (this.CorrectForNesting) {
+				threadStopwatches.Add (threadId, stopwatch);
+			}
+
+			stopwatch.Start ();
+			runMeasurementSubject ();
+			stopwatch.Stop ();
+
+			if (this.CorrectForNesting) {
+				threadStopwatches.Remove (threadId);
+			}
+
+			if (unstackProfiler) {
+				threadStopwatches.Add (threadId, previousStopwatch);
+				previousStopwatch.Start ();
+			}
+
+			Interlocked.Add (ref ticksSpent, stopwatch.ElapsedTicks);
+			Interlocked.Increment (ref measurementsMade);
+		}
+	}
+}
