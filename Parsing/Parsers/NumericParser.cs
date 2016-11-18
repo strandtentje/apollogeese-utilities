ce8c89449402b03a5f3100b0c2ce460ec0eed751
Rob Tierolff <teh@borreh.nl>
2016-11-18 14:39:36 +0100

    Parser for any numeric value

---
+++ Parsing/Parsers/NumericParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers
+{
+	public class NumericParser : AnyParser
+	{
+		private static bool floatParse(string data, out float value)
+		{
+			return float.TryParse(
+				data, 
+				NumberStyles.Float, 
+				CultureInfo.InvariantCulture.NumberFormat, 
+				out value);
+		}
+
+		private static bool decimalParse(string data, out decimal value) {
+			value = 0;
+			if (data.StartsWith ("!")) {
+				return decimal.TryParse (
+					data.Substring (1), 
+					NumberStyles.Currency, 
+					CultureInfo.InvariantCulture.NumberFormat,
+					out value);
+			} else {
+				return false;
+			}
+		}
+
+		public NumericParser (): base (
+			new ValueParser<decimal> (decimalParse),
+			new ValueParser<int> (int.TryParse), 
+			new ValueParser<long> (long.TryParse),
+			new ValueParser<float> (floatParse)
+		) { }
+	}
+}
+
