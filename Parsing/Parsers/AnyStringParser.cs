e54afc3ce88ade2c062fbf0e42652def02ae9bbd
Rob Tierolff <teh@borreh.nl>
2016-11-18 14:38:53 +0100

    Parser for string, filename or reference thereto

---
+++ Parsing/Parsers/AnyStringParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers
+{
+	public class AnyStringParser : AnyParser
+	{
+		public AnyStringParser () : base (
+			new FilenameParser (),
+			new ReferenceParser (),
+			new StringParser ()
+		) { }
+	}
+}
+
