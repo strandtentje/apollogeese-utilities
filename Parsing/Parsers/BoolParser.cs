025923b540363b9500575455ec57ce237675c9c3
Rob Tierolff <teh@borreh.nl>
2016-11-18 14:39:29 +0100

    Boolean value parser

---
+++ Parsing/Parsers/BoolParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers
+{
+	public class BoolParser : ValueParser<bool>
+	{
+		public BoolParser (): base(bool.TryParse, "(True|False|true|false)")
+		{
+		}
+	}
+}
+
