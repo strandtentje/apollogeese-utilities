378854dd868d1a4ef9970ed75696000964199739
Rob Tierolff <teh@borreh.nl>
2016-11-18 14:40:39 +0100

    Static helping class for loading settings from file

---
+++ Parsing/Parsers/Settings/SettingsLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BorrehSoft.Utensils.Log;
+using System.IO;
+using BorrehSoft.Utensils.Parsing;
+using BorrehSoft.Utensils.Parsing.Parsers;
+
+namespace BorrehSoft.Utensils.Collections.Settings
+{
+	public static class SettingsLoader
+	{
+		/// <summary>
+		/// Acquires settings from the file.
+		/// </summary>
+		/// <returns>The file.</returns>
+		/// <param name="file">File.</param>
+		public static Settings FromFile(string file, string workingDirectory = null)
+		{
+			Secretary.Report (5, "Loading settings file ", file);
+
+			if (!File.Exists (file)) {
+				File.WriteAllText(file, "{}");
+				Secretary.Report (5, file, " didn't exist. Has been created.");
+			}
+
+			ParsingSession session = ParsingSession.FromFile(file, new IncludeParser());
+
+			if (workingDirectory == null) {
+				Directory.SetCurrentDirectory (session.SourceFile.Directory.FullName);
+			} else if (Directory.Exists(workingDirectory)) {
+				Directory.SetCurrentDirectory(workingDirectory);
+				session.WorkingDirectory = workingDirectory;
+			}
+
+			SettingsParser parser = new SettingsParser();
+			object result;
+
+			Settings config;
+
+			if (parser.Run (session, out result) < 0)
+				config = new Settings ();
+			else 
+				config = (Settings)result;
+
+			config.SourceFile = session.SourceFile;
+
+			Secretary.Report (5, "Settings finished loading from: ", file);
+
+			// Secretary.Report (6, session.ParsingProfiler.FinalizeIntoReport().ToString());
+
+			return config;
+		}
+	}
+}
+
