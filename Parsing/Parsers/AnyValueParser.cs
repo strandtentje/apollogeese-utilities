08311843879657baaad3ec08acfd85e4816c4ad8
Rob Tierolff <teh@borreh.nl>
2016-11-18 14:38:11 +0100

    Parser for any quantifiable value

---
+++ Parsing/Parsers/AnyValueParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers
+{
+	public class AnyValueParser : AnyParser
+	{
+		public AnyValueParser () : base(new NumericParser(), new BoolParser())
+		{
+		}
+	}
+}
+
