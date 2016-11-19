84afbbc6fab0595a7d410fa4f69db952eeaaec89
Rob Tierolff <teh@borreh.nl>
2016-11-19 18:46:11 +0100

    Unit tests for parser

---
+++ Parsing.Unit/StringParserTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorrehSoft.Utilities.Parsing.Unit
+{
+	public class StringParserTest
+	{
+		public StringParserTest ()
+		{
+		}
+	}
+}
+
---
+++ Parsing.Unit/WhitespaceParserTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using BorrehSoft.Utilities.Parsing.Parsers;
+
+namespace BorrehSoft.Utilities.Parsing.Unit
+{
+	[TestFixture ()]
+	public class WhitespaceParserTest
+	{
+		WhitespaceParser SUT;
+
+		[Test()]
+		public void SetUp()
+		{
+			this.SUT = new WhitespaceParser ();
+		}
+
+		[Test ()]
+		public void TestSingleSpace ()
+		{
+			Assert.AreEqual (" ", Parse<string>.Using (this.SUT, " A"));
+		}
+
+		[Test()]
+		public void TestDoubleSpace() 
+		{
+			Assert.AreEqual ("  ", Parse<string>.Using (this.SUT, "  A"));
+		}
+
+		[Test()]
+		public void TestComplex()
+		{
+			Assert.AreEqual ("  \r\n\t  ", Parse<string>.Using (this.SUT, "  \r\n\t  A"));
+		}
+	}
+}
+
