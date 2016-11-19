082f8ff3a96fb535d2dad17abbc52d4eace6e0a9
Rob Tierolff <teh@borreh.nl>
2016-11-19 17:44:25 +0100

    CLON syntax definition

---
+++ Parsing/Parsers/Settings/SettingsSyntax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers.SettingsParsers
+{
+	public class SettingsSyntax
+	{
+		public virtual char BodyOpen { get { return '{'; } }
+		public virtual char BodyClose { get { return '}'; } }
+		public virtual char BodyDelimiter { get { return ';'; } }
+
+		public virtual string BodyShorthand { get { return "->"; } }
+		public virtual string BodySubstitution { get { return "_branch"; } }
+		public virtual char BodyAssign { get { return '='; } }
+
+		public virtual string ConfigurationKey { get { return "modconf"; } }
+
+		public virtual string HeadMarker { get { return "_configline"; } }
+		public virtual string HeadIdentifier { get { return "type"; } }
+		public virtual string HeadAnonymousValueKey { get { return "default"; } }
+
+		public virtual char HeadOpen { get { return '('; } }
+		public virtual char HeadClose { get { return ')'; } }
+		public virtual char HeadDelimiter { get { return ','; } }
+
+		public virtual char AnonymousHeadOpener { get { return '<'; } }
+		public virtual char AnonymousHeadCloser { get { return '>'; } }
+		public virtual string AnonymousHeadName { get { return "Module"; } }
+
+		public virtual string HeadShorthand { get { return "<-"; } }
+		public virtual string HeadSubstitution { get { return "_override"; } }
+		public virtual char HeadAssign { get { return '='; } }
+
+		public virtual char StringConcatenationOpen { get { return '/'; } }
+		public virtual char StringConcatenationClose { get { return '/'; } }
+		public virtual char StringConcatenationDelimiter { get { return '|'; } }
+
+		public virtual char ArrayOpen { get { return '['; } }
+		public virtual char ArrayClose { get { return ']'; } }
+		public virtual char ArrayDelimiter { get { return ','; } }
+
+		public virtual char Chainer { get { return ':'; } }
+		public virtual char Successor { get { return '&'; } }
+		public virtual string ChainSubstitution { get { return "_with_branch"; } }
+		public virtual string SuccessorSubstitution { get { return "_successor_branch"; } }
+	}
+}
+
