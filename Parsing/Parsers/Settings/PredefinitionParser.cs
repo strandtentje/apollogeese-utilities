59b74747f78906ec834a72d742c022900f309407
Rob Tierolff <teh@borreh.nl>
2016-11-19 17:48:24 +0100

    Parser for separate inherit references after head

---
+++ Parsing/Parsers/Settings/PredefinitionParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using BorrehSoft.Utensils.Collections.Settings;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers.SettingsParsers
+{
+	public class PredefinitionParser : Parser
+	{
+		ReferenceParser ReferenceParser = new ReferenceParser ();
+
+		public PredefinitionParser ()
+		{
+		}
+
+		internal override int ParseMethod (ParsingSession session, out object result)
+		{
+			List<Settings> predefinitions = new List<Settings> ();
+
+			object referenceCandidate;
+			while (ReferenceParser.ParseMethod (session, out referenceCandidate) > 0) {
+				if (referenceCandidate is Settings) {
+					predefinitions.Add ((Settings)referenceCandidate);
+				} else {
+					throw new ParsingException (session, ReferenceParser, session.Ahead, session.Trail);
+				}
+			} 
+
+			result = predefinitions;
+			return predefinitions.Count + 1;
+		}
+	}
+}
+
