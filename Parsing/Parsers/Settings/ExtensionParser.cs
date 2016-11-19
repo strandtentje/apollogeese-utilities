192bfe382fea82c737fa738f5185ff7ea154caee
Rob Tierolff <teh@borreh.nl>
2016-11-19 17:48:45 +0100

    Parser for parsing definitions after characters

---
+++ Parsing/Parsers/Settings/ExtensionParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BorrehSoft.Utensils.Collections.Settings;
+
+namespace BorrehSoft.Utensils.Parsing.Parsers.SettingsParsers
+{
+	public class ExtensionParser : Parser
+	{
+		CharacterParser ExtensionMarker;
+		ReferenceParser ReferenceParser;
+		Parser InnerParser;
+
+		public ExtensionParser(char marker, Parser innerParser)
+		{
+			ExtensionMarker = new CharacterParser (marker);
+			ReferenceParser = new ReferenceParser ();
+			InnerParser = innerParser;
+		}
+
+		internal override int ParseMethod (ParsingSession session, out object result)
+		{
+			if (Parse<char>.Try (ExtensionMarker, session)) {
+				var chainDefinition = 
+					Parse<Settings>.Using (ReferenceParser, session) ??
+					Parse<Settings>.Using (InnerParser, session);
+
+				if (chainDefinition == null) {
+					throw new ParsingException (session, InnerParser, session.Ahead, session.Trail);
+				}
+
+				result = chainDefinition;
+				return 1;
+			} else {
+				result = null;
+				return -1;
+			}
+		}
+	}
+}
+
