89df175b9946489765de1f4270e5f68b893815c9
Rob Tierolff <teh@borreh.nl>
2016-11-20 02:08:37 +0100

    Filename unit test

---
+++ Parsing.Unit/FilenameParserTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using BorrehSoft.Utilities.Parsing.Parsers;
+using System.IO;
+
+namespace BorrehSoft.Utilities.Parsing.Unit
+{
+	[TestFixture()]
+	public class FilenameParserTest : SessionTest
+	{
+		FilenameParser SUT;
+
+		[SetUp()]
+		public void SetUp()
+		{
+			this.SUT = new FilenameParser();
+		}
+
+		[Test()]
+		public void TestPicksUpRelativeFile()
+		{
+			var session = GivenSessionWithText ("f\"tosti.txt\"");
+			session.WorkingDirectory = "kaas";
+			Assert.AreEqual ("kaas" + Path.DirectorySeparatorChar + "tosti.txt", Parse<string>.Using (SUT, session));
+		}
+
+		[Test()]
+		public void TestIgnoresString()
+		{
+			var session = GivenSessionWithText ("\"tosti.txt\"");
+			Assert.AreEqual ("", Parse<string>.Using (SUT, session));
+		}
+
+		[Test()]
+		public void TestIgnoresFIdentifier()
+		{
+			var session = GivenSessionWithText ("fKaasTosti");
+			Assert.AreEqual ("", Parse<string>.Using (SUT, session));
+		}
+	}
+}
+
